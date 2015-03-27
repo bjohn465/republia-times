d19f74c6eb19fd9b86459c7593cc29a588311540
Brandon Johnson <bjohn465+gihub@gmail.com>
2015-03-27 14:28:37 -0600

    Add day number to game state

--- src/components/republia-times.js
+++ src/components/republia-times.js
@@ -3,6 +3,12 @@ var React = require( "react" );
 module.exports = React.createClass({
 	displayName: "RepubliaTimes",
 
+	getInitialState() {
+		return {
+			day: 1
+		};
+	},
+
 	render() {
 		return (
 			<h1>The Republia Times</h1>
