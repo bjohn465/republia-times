b43db04a9a1fd99d88b85f363548996ee06f5e8d
Brandon Johnson <bjohn465+gihub@gmail.com>
2015-03-27 14:44:13 -0600

    Add basic structure to morning screen

--- src/components/morning-screen.js
+++ src/components/morning-screen.js
@@ -10,8 +10,22 @@ module.exports = React.createClass({
 	render() {
 		return (
 			<div>
-				<h1>The Republia Times</h1>
+				<header>
+					<h1>The Republia Times</h1>
+				</header>
 				<h2>Day {this.props.day}</h2>
+				<p>TODO: Message</p>
+				<button type="button">Start Work</button>
+				<footer>
+					<div>by</div>
+					<div>Lucas Pope</div>
+					<div><a href="https://twitter.com/dukope">@dukope</a></div>
+					<div>Ported by</div>
+					<div>Brandon Johnson</div>
+					<div>
+						<a href="https://twitter.com/bjohn465">@bjohn465</a>
+					</div>
+				</footer>
 			</div>
 		);
 	}
