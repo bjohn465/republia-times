31896cec3bfb7aa9fcf50127b0fc23b0cad01b27
Brandon Johnson <bjohn465+gihub@gmail.com>
2015-04-01 15:40:00 -0600

    Add simple time rendering

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,13 +1,32 @@
 var React = require( "react/addons" );
+var PropTypes = React.PropTypes;
+
+var leadingZero = function( num ) {
+	return ( num < 10 ? "0" : "" ) + num;
+};
+
+var formatTime = function( timeObj ) {
+	return leadingZero( timeObj.hours ) + ":" +
+		leadingZero( timeObj.minutes ) + ":" +
+		leadingZero( timeObj.seconds );
+};
 
 module.exports = React.createClass({
 	displayName: "Clock",
 
 	mixins: [ React.addons.PureRenderMixin ],
 
+	propTypes: {
+		time: PropTypes.shape({
+			hours: PropTypes.number.isRequired,
+			minutes: PropTypes.number.isRequired,
+			seconds: PropTypes.number.isRequired
+		}).isRequired
+	},
+
 	render() {
 		return (
-			<div>Clock</div>
+			<time>{formatTime( this.props.time )}</time>
 		);
 	}
 });
