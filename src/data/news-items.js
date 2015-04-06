26d6f2fa98fb540d3681ee9e1c402f564976a9e1
Brandon Johnson <bjohn465+gihub@gmail.com>
2015-04-06 12:33:52 -0600

    Add weather stories

--- src/data/news-items.js
+++ src/data/news-items.js
@@ -1,7 +1,7 @@
 const Immutable = require( "immutable" );
 const LOYALTY_NONE = 0;
 
-function createWeatherStory( blurbText, articleText ) {
+function createWeatherStory( [ blurbText, articleText ] ) {
 	return Immutable.Map({
 		loyalty: LOYALTY_NONE,
 		interesting: true,
@@ -9,3 +9,52 @@ function createWeatherStory( blurbText, articleText ) {
 		articleText
 	});
 }
+
+const weatherStories = Immutable.Set(
+	[
+		[
+			"Skies and temperatures will remain calm today",
+			"Another Sunny Day!"
+		],
+		[
+			"Storms predicted to wash western coast out to sea",
+			"Western Storms Threaten Coast!"
+		],
+		[
+			"Forecast expects heavy rains in the north and east",
+			"Showers Rain Down!"
+		],
+		[
+			"Expect unseasonal snow in the south",
+			"Blizzard Incoming?"
+		],
+		[
+			"Sunny morning and cloudy evening for the day",
+			"Warm To Cloudy!"
+		],
+		[
+			"Light showers throughout the day",
+			"Warm To Cloudy This Week!"
+		],
+		[
+			"Hurricane-level winds spotted off eastern coast",
+			"Eastern Hurricanes Return!"
+		],
+		[
+			"Clear skies and no sign of rain",
+			"Another Dry Day!"
+		],
+		[
+			"Freezing sleet and snow expected in northern mountains",
+			"Buckle Down For Ice!"
+		],
+		[
+			"Tropical breezes blow across southeastern coast",
+			"Sea Breeze Incoming!"
+		]
+	].map( createWeatherStory )
+);
+
+const allStories = Immutable.Set().concat( weatherStories );
+
+module.exports = allStories;
