d4850679835cd44d3cae80b46c413a20dab94937
Brandon Johnson <bjohn465+gihub@gmail.com>
2017-09-30 14:19:44 -0600

    upgrade to enzyme 3.0.0

---
+++ jest/setup.js
@@ -0,0 +1,6 @@
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-15'
+
+Enzyme.configure({ adapter: new Adapter() })
