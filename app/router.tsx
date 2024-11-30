4f2a5d992a5cd267aea67620a8fba0ccace1f858
Brandon Johnson <bjohn465+github@gmail.com>
2024-11-30 15:33:30 -0700

    Remove v7 future flags

--- app/router.tsx
+++ app/router.tsx
@@ -2,20 +2,10 @@ import { createHashRouter } from 'react-router-dom'
 import ErrorPage from './error-page.tsx'
 import Root from './routes/root.tsx'
 
-export const router = createHashRouter(
-	[
-		{
-			path: '/',
-			element: <Root />,
-			errorElement: <ErrorPage />,
-		},
-	],
+export const router = createHashRouter([
 	{
-		future: {
-			v7_fetcherPersist: true,
-			v7_normalizeFormMethod: true,
-			v7_partialHydration: true,
-			v7_relativeSplatPath: true,
-		},
+		path: '/',
+		element: <Root />,
+		errorElement: <ErrorPage />,
 	},
-)
+])
