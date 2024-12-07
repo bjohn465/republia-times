c68d1117166259fa809633bd2baf49dcda825f25
Brandon Johnson <bjohn465+github@gmail.com>
2024-12-07 16:19:56 -0700

    Import StrictMode and createRoot directly

--- app/entry.tsx
+++ app/entry.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import { router } from './router.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
 		<RouterProvider router={router} />
-	</React.StrictMode>,
+	</StrictMode>,
 )
