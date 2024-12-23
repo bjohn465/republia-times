cfbe8db4c01c79afbe0f462c44e8293ebaf66cb0
Brandon Johnson <bjohn465+github@gmail.com>
2024-12-23 15:06:11 -0700

    Display error message when initialization fails
    The error will be displayed
    if the root element is missing
    (this should not happen,
    but we check for it anyway)
    or if the locale fails to load
    (since it is loaded dynamically,
    that means the request
    could potentially fail).

--- app/entry.tsx
+++ app/entry.tsx
@@ -5,14 +5,19 @@ import { detectLocale, dynamicActivate } from './i18n.ts'
 import { router } from './router.tsx'
 
 async function entry() {
+	function addErrorClass() {
+		document.documentElement.classList.add('initializationError')
+	}
 	const root = document.getElementById('root')
 	if (!root) {
+		addErrorClass()
 		throw new Error('The root element is missing')
 	}
 	const locale = detectLocale()
 	try {
 		await dynamicActivate(locale)
 	} catch (ex) {
+		addErrorClass()
 		throw new Error(`There was a problem loading the locale "${locale}"`, {
 			cause: ex,
 		})
--- tests/e2e/playthrough.test.ts
+++ tests/e2e/playthrough.test.ts
@@ -26,8 +26,23 @@ test('Initial load', async ({ page }) => {
 
 	await page.goto('/', { waitUntil: 'commit' })
 	await expect(page.getByText('Loading...')).toBeVisible()
+	await expect(
+		page.getByText('There was a problem loading the game.'),
+	).not.toBeVisible()
 	await jsDelay.promise
 	await expect(page.getByText('Loading...')).not.toBeVisible()
+})
+
+test('Error loading locale', async ({ page }) => {
+	// This is the locale in the dev environment
+	await page.route(/\/locales\/en\.po/, (route) => route.abort('failed'))
+	// And this is the locale in the production environment
+	await page.route(/en-[A-Za-z0-9]+?\.js$/, (route) => route.abort('failed'))
+	await page.goto('/')
+	await expect(page.getByText('Loading...')).not.toBeVisible()
+	await expect(
+		page.getByText('There was a problem loading the game.'),
+	).toBeVisible()
 })
 
 test('Game start', async ({ page }) => {
