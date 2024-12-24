f9c1879c856934b10c75f8df4c5ba3d6a4b0d096
Brandon Johnson <bjohn465+github@gmail.com>
2024-12-24 10:59:33 -0700

    Add day route and basic component

--- app/router.tsx
+++ app/router.tsx
@@ -1,5 +1,6 @@
 import { createHashRouter } from 'react-router'
 import ErrorPage from './error-page.tsx'
+import Day from './routes/day.component.tsx'
 import Morning from './routes/morning.component.tsx'
 import Root from './routes/root.component.tsx'
 import { loader as rootLoader } from './routes/root.data.ts'
@@ -14,6 +15,10 @@ export const router = createHashRouter([
 			{
 				path: 'morning',
 				Component: Morning,
+			},
+			{
+				path: 'day',
+				Component: Day,
 			},
 		],
 	},
---
+++ app/routes/day.component.tsx
@@ -0,0 +1,9 @@
+import { Trans } from '@lingui/react/macro'
+
+export default function Day() {
+	return (
+		<h1>
+			<Trans>Day 1</Trans>
+		</h1>
+	)
+}
