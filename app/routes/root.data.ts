26c3ebef7687dd4d69d2287d0e0f8362e37a0940
Brandon Johnson <bjohn465+github@gmail.com>
2024-12-24 10:55:45 -0700

    Change initial redirect to replace
    So we do not keep
    a history entry
    for the initial page,
    which does not have
    anything on it.

--- app/routes/root.data.ts
+++ app/routes/root.data.ts
@@ -1,9 +1,9 @@
-import { type LoaderFunctionArgs, redirect } from 'react-router'
+import { type LoaderFunctionArgs, replace } from 'react-router'
 
 export function loader({ request }: LoaderFunctionArgs) {
 	const url = new URL(request.url)
 	if (url.pathname === '/') {
-		throw redirect('/morning')
+		throw replace('/morning')
 	}
 	return null
 }
