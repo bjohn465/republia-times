cd74bfd10b1698092c7b39fac48435d0065cea6f
Brandon Johnson <bjohn465+github@gmail.com>
2025-01-11 16:17:40 -0700

    Use functions for text of news items
    The `t` tag function from lingui needs to run
    after lingui has been set up with the correct language,
    so it has to be called from within a function
    that runs after that setup happens.
    
    Previously, we used a `generateNewsItems` function
    to populate the news items in the `newsItemsByID` map
    if a call to `getNewsItems` determined that the map was empty.
    This does not work if the new `getNewsItem` function
    is called before `getNewsItems` has been called.
    
    Instead, we will use two different types of news items:
    
     - One news item type will use functions for the item text.
       The function, when called, returns the translated text.
     - The second type of news item has the translated text.
    
    The data load will be the boundary
    between the two types of news items.
    So the UI code that gets its data from the loader
    will always have the translated text.

--- app/news-items.ts
+++ app/news-items.ts
@@ -1,45 +1,72 @@
+import { invariant } from '@epic-web/invariant'
 import { t } from '@lingui/core/macro'
 import * as v from 'valibot'
-
-const newsItemsByID = new Map<NewsItemID, Readonly<NewsItem>>()
-
-export function getNewsItems() {
-	if (newsItemsByID.size === 0) {
-		generateNewsItems()
-	}
-	return Object.freeze(Array.from(newsItemsByID.values()))
-}
-
-function generateNewsItems() {
-	const newsItems: NewsItem[] = [
-		{
-			id: newsItemID`bBQb`,
-			feedText: t`Tennis star Restojiu powers through semifinal brackets`,
-			articleText: t`Tennis Star Advances!`,
-		},
-		{
-			id: newsItemID`9MrF`,
-			feedText: t`Mega-group HugginBoyz admits to not singing on any albums, can barely dance`,
-			articleText: t`HugginBoyz: Talentless After All!`,
-		},
-	]
-	newsItems.forEach((item) => {
-		newsItemsByID.set(item.id, Object.freeze(item))
-	})
-}
-
-function newsItemID(strings: TemplateStringsArray): NewsItemID {
-	return v.parse(NewsItemIDSchema, String.raw({ raw: strings }))
-}
 
 // Use a "branded type" for the news item ID,
 // so not just any string is used for an ID.
 const NewsItemIDSchema = v.pipe(v.string(), v.brand('NewsItemID'))
 type NewsItemID = v.InferOutput<typeof NewsItemIDSchema>
 
-export const NewsItemSchema = v.object({
+const PreTranslatedTextSchema = v.pipe(
+	v.function(),
+	v.args(v.tuple([])),
+	v.returns(v.string()),
+)
+type PreTranslatedText = v.InferOutput<typeof PreTranslatedTextSchema>
+type PreTranslatedTextReturn = ReturnType<PreTranslatedText>
+const PreTranslatedTextTransform = v.transform<
+	PreTranslatedText,
+	PreTranslatedTextReturn
+>((preTranslatedText) => preTranslatedText())
+
+export const PreTranslatedNewsItemSchema = v.object({
 	id: NewsItemIDSchema,
-	feedText: v.string(),
-	articleText: v.string(),
+	feedText: PreTranslatedTextSchema,
+	articleText: PreTranslatedTextSchema,
+})
+export type PreTranslatedNewsItem = v.InferOutput<
+	typeof PreTranslatedNewsItemSchema
+>
+
+export const NewsItemSchema = v.object({
+	...PreTranslatedNewsItemSchema.entries,
+	feedText: v.pipe(
+		PreTranslatedNewsItemSchema.entries.feedText,
+		PreTranslatedTextTransform,
+	),
+	articleText: v.pipe(
+		PreTranslatedNewsItemSchema.entries.articleText,
+		PreTranslatedTextTransform,
+	),
 })
 export type NewsItem = v.InferOutput<typeof NewsItemSchema>
+
+const newsItems: readonly Readonly<PreTranslatedNewsItem>[] = [
+	{
+		id: newsItemID`bBQb`,
+		feedText: () => t`Tennis star Restojiu powers through semifinal brackets`,
+		articleText: () => t`Tennis Star Advances!`,
+	},
+	{
+		id: newsItemID`9MrF`,
+		feedText: () =>
+			t`Mega-group HugginBoyz admits to not singing on any albums, can barely dance`,
+		articleText: () => t`HugginBoyz: Talentless After All!`,
+	},
+]
+
+const newsItemsByID = new Map(newsItems.map((item) => [item.id, item]))
+
+export function getNewsItems() {
+	return newsItems
+}
+
+export function getNewsItem(id: NewsItemID) {
+	const item = newsItemsByID.get(id)
+	invariant(item, `Unable to find news item with ID ${id}`)
+	return item
+}
+
+function newsItemID(strings: TemplateStringsArray): NewsItemID {
+	return v.parse(NewsItemIDSchema, String.raw({ raw: strings }))
+}
--- app/routes/day.data.ts
+++ app/routes/day.data.ts
@@ -2,6 +2,12 @@ import { getNewsItems } from '#app/news-items.ts'
 
 export function loader() {
 	return {
-		newsItems: getNewsItems(),
+		newsItems: getNewsItems().map((item) => ({
+			...item,
+			// Return translated text
+			// instead of the function that returns the translated text.
+			feedText: item.feedText(),
+			articleText: item.articleText(),
+		})),
 	}
 }
