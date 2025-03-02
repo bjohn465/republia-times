b5f65b3f3a7165133f62b7444127489e9f791445
Brandon Johnson <bjohn465+github@gmail.com>
2025-03-02 07:44:56 -0700

    Remove previous game state implementation
    So we're only using the new class-based game states.

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,6 +1,5 @@
 import { type DeepPartial } from 'utility-types'
 import { GameScreen } from '#app/state/game-screen.ts'
-import { type DayStateInput, type MorningState } from '#app/state/game-state.ts'
 
 export function getMorningStateInput(
 	overrides?: DeepPartial<{ screen: string }>,
@@ -24,32 +23,6 @@ export function getDayStateInput(
 		paper: { articles: [] },
 	}
 	return { ...defaults, ...overrides }
-}
-
-export function getMorningGameState(overrides?: Partial<MorningState>) {
-	const defaults: MorningState = {
-		screen: GameScreen.Morning,
-		v: 1,
-	}
-	return {
-		...defaults,
-		...overrides,
-	}
-}
-
-export function getDayGameStateInput(overrides?: Partial<DayStateInput>) {
-	const defaults: DayStateInput = {
-		newsItems: ['bBQb', '9MrF'],
-		screen: GameScreen.Day,
-		paper: {
-			articles: [],
-		},
-		v: 1,
-	}
-	return {
-		...defaults,
-		...overrides,
-	}
 }
 
 export function encodeGameStateURLParamValue(value: string): string {
