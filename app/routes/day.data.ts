93cc76f077b43c0da81604cf453c47bf0a3c4e17
Brandon Johnson <bjohn465+github@gmail.com>
2025-08-22 12:43:21 -0600

    Remove workaround for serializing ReadonlyMap
    React Router v7.8.2 contains the fix
    for serializing `ReadonlyMap` and `ReadonlySet` types
    that someone going by @bjohn465 wrote
    (see https://github.com/remix-run/react-router/pull/13092).
    That means this workaround is no longer needed! 🎉

--- app/routes/day.data.ts
+++ app/routes/day.data.ts
@@ -1,8 +1,4 @@
-import {
-	type ActionFunctionArgs,
-	type unstable_SerializesTo as SerializesTo,
-	type useLoaderData,
-} from 'react-router'
+import { type ActionFunctionArgs } from 'react-router'
 import * as v from 'valibot'
 import { parseFormData } from '#app/form-data.ts'
 import { Intents, intentSchema } from '#app/intents.ts'
@@ -19,7 +15,7 @@ export function loader() {
 		'Invalid game state for "day" loader',
 	)
 	return {
-		newsItems: toSerializableReadonlyMap(gameState.newsItems),
+		newsItems: gameState.newsItems,
 		paper: dehydratePaper(gameState.paper),
 	}
 }
@@ -70,26 +66,3 @@ const DayActionFormDataSchema = v.variant('intent', [
 		id: NewsItemIdSchema,
 	}),
 ])
-
-/**
- * A workaround for https://github.com/remix-run/react-router/issues/13092
- * that casts a `ReadonlyMap` to a `SerializesTo<ReadonlyMap>`
- * so the map's type serializes correctly when the route loader data is used
- * via `useLoaderData`.
- * @param readonlyMap The `ReadonlyMap` to cast
- * @returns The `ReadonlyMap` as a `SerializesTo<ReadonlyMap>`
- */
-function toSerializableReadonlyMap<T>(
-	readonlyMap: SerializableReadonlyMap<T>,
-): SerializesTo<T> {
-	return readonlyMap as unknown as SerializesTo<T>
-}
-
-type SerializableReadonlyMap<T> =
-	// Get the types of the keys and values
-	T extends ReadonlyMap<infer K, infer V>
-		? // Ensure `T` as a `Map` serializes
-			ReturnType<typeof useLoaderData<() => Map<K, V>>> extends Map<K, V>
-			? T
-			: never
-		: never
